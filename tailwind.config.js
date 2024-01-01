869cb5ce6ea10d8e6ef6f155b165b8c33609f789
rkfbrn <kerenr445@gmail.com>
2024-01-01 21:17:50 +0700

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      primary: '#344E31',
+      dark: '#14532d',
+      secondary: '#62795B',
+      text: '#D2E3C8'
+    },
   },
   plugins: [],
 }
