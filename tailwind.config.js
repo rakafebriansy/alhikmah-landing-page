cc57c4df1f9c934cd0487d5bf5f55a6ed04974a4
rkfbrn <kerenr445@gmail.com>
2024-01-04 17:14:23 +0700

    masukan asset

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,7 @@ module.exports = {
   content: ["index.html"],
   theme: {
     extend: {
-      primary: '#344E31',
-      dark: '#14532d',
-      secondary: '#62795B',
-      text: '#D2E3C8'
+
     },
   },
   plugins: [],
