ecc86918474f9de65c3bb0d48c619bfe860f6087
rkfbrn <kerenr445@gmail.com>
2024-01-06 19:44:08 +0700

    progress1 - raka

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
   content: ["index.html"],
   theme: {
     extend: {
-
+      colors: {
+        dark: {
+          primary: '#344D2F',
+          secondary: '#647E5D',
+          text: '#DDF8D6',
+        }
+      },
     },
   },
   plugins: [],
